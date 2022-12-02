e186b5fd9e9eca1931667eb572a49eab0195c886
HossainRana <58656900+HossainRana@users.noreply.github.com>
2022-12-03 04:58:16 +0600

    fix and new page

---
+++ resources/js/dashboard.js
@@ -0,0 +1,138 @@
+$(document).ready(function (){
+
+
+
+    /*Bar Chart Start*/
+
+
+    var barChartData = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        datasets: [
+            {
+                label: 'Maintenance',
+                backgroundColor: "#EDE7F6",
+                data: [140,
+                    691,
+                    636,
+                    662,
+                    686,
+                    668
+                ]
+            },{
+                label: 'Investment',
+                backgroundColor: "#90CAF9",
+                data: [140,
+                    180,
+                    636,
+                    662,
+                    686,
+                    668
+                ]
+            }, {
+                label: 'Loss',
+                backgroundColor: "##2196F3",
+                data: [140,
+                    50,
+                    156,
+                    184,
+                    186,
+                    142
+                ]
+            }, {
+                label: 'Profit',
+                backgroundColor: "#673AB7",
+                data: [140,
+                    200,
+                    166,
+                    125,
+                    112,
+                    122
+                ]
+            }]
+    };
+
+    var barchart = document.getElementById("barChart").getContext("2d");
+    window.myBar = new Chart(barchart, {
+        responsive:true,
+        type: 'bar',
+        data: barChartData,
+        options: {
+            title: {
+                display: true,
+                text: "",
+            },
+            legend: {
+                position: "bottom"
+            },
+            tooltips: {
+                mode: 'label'
+            },
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                    barPercentage: 0.4
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    });
+
+
+    /*Bar Chart End*/
+
+    /*Line Chart Start*/
+
+    var linechart = document.getElementById('myChart').getContext("2d");
+
+    var myChart = new Chart(linechart, {
+        type: 'line',
+        data: {
+            labels: ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL"],
+            datasets: [{
+                label: "Data",
+                borderColor: "#80b6f4",
+                pointBorderColor: "#80b6f4",
+                pointBackgroundColor: "#80b6f4",
+                pointHoverBackgroundColor: "#5E35B1",
+                pointHoverBorderColor: "#80b6f4",
+                pointBorderWidth: 0,
+                pointHoverRadius: 5,
+                pointHoverBorderWidth: 1,
+                pointRadius: 1,
+                fill: true,
+                borderWidth: 1,
+                data: [10, 12, 15, 17, 18, 17, 16]
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        display: false,
+                    },
+                    gridLines: {
+                        drawTicks: false,
+                        display: false
+                    }
+
+                }],
+                xAxes: [{
+                    gridLines: {
+                        zeroLineColor: "transparent"
+                    },
+                    ticks: {
+                        display: false,
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    /*Line Chart End*/
+
+})
---
+++ resources/js/site.js
@@ -0,0 +1,63 @@
+$(document).ready(function (){
+    /*dashoabrd*/
+    var profileMenu = document.getElementById("profile");
+    profileMenu.onclick = function () {
+        var menuList = document.getElementById("profileMenu");
+        menuList.classList.toggle("menu__show");
+    };
+
+    /*Left dropdown menu*/
+    var sidebar = document.getElementById("dropdown");
+    var menuList = document.getElementById("dropdownMenu");
+    sidebar.onclick = function () {
+        menuList.classList.toggle("menu__show");
+        document.querySelector("#dropdown > svg").classList.toggle('rotate')
+    };
+
+    /*Left menu*/
+    var navMenu = document.getElementById("navBar");
+    navMenu.onclick = function () {
+        var menuList = document.getElementById("leftMenu");
+        menuList.classList.toggle("navmenu__show");
+    };
+
+    /*Menu bar*/
+
+    document.querySelector("#navBar").addEventListener('click', function () {
+        document.querySelector("#navBar").classList.toggle('cross__menu--bar')
+    })
+
+    /*Breadcrumb*/
+    let text = document.querySelector('.active_breadcrumb');
+    document.getElementById('nav-profile-tab').addEventListener('click', function (){
+        text.innerHTML ='Pending';
+    })
+    document.getElementById('nav-contact-tab').addEventListener('click', function (){
+        text.innerHTML ='Uploaded';
+    })
+    document.getElementById('nav-home-tab').addEventListener('click', function (){
+        text.innerHTML ='All';
+    })
+
+    let breadCrumb = document.querySelector('.employee_breadcrumb');
+    document.getElementById('all-employee-tab').addEventListener('click', function (){
+        alert('test')
+        breadCrumb.innerHTML ='All Employees';
+    })
+    document.getElementById('visa-tab').addEventListener('click', function (){
+        breadCrumb.innerHTML ='Visa Status';
+    })
+    document.getElementById('uploded-tab').addEventListener('click', function (){
+        breadCrumb.innerHTML ='Uploaded';
+    })
+
+    /*Action bar*/
+    document.querySelector('.sync__payroll').addEventListener('click', function (){
+        document.querySelector('.payroll__sync--container').classList.toggle('sync__payroll--active')
+    })
+    document.querySelector('.recent__invitation').addEventListener('click', function (){
+        document.querySelector('.invitation__recent--container').classList.toggle('recent__invitation--active')
+    })
+
+
+})
