9c06eefca8a07fe7e83d922e8940e264ac82a9ee
HossainRana <rana.cseewu@gmail.com>
2022-12-07 18:10:49 +0600

    filter options

--- resources/js/site.js
+++ resources/js/site.js
@@ -60,21 +60,75 @@ $(document).ready(function (){
     })
 
 
+    /*filter*/
 
     document.querySelector('.payroll__filter--container').addEventListener('click', function (e){
-        if (e.target.classList.contains('filter__btn')){
-            if (!e.target.classList.contains('active__filter--option')){
-                document.querySelector('.filter__btn').classList.remove('active__filter--option')
-            }
-            e.target.classList.toggle('active__filter--option');
+        if (e.target.classList.contains('job__filter--btn')){
+            const showJobFilter = document.querySelector('.job__list');
+            showJobFilter.classList.toggle('show__job')
+        }
+    })
+    document.querySelector('.payroll__filter--container').addEventListener('click', function (e){
+        if (e.target.classList.contains('venues__filter--btn')){
+            const showVenuesFilter = document.querySelector('.venus__list');
+            showVenuesFilter.classList.toggle('show__venues')
+        }
+    })
+    document.querySelector('.payroll__filter--container').addEventListener('click', function (e){
+        if (e.target.classList.contains('sync__filter--btn')){
+            const showVenuesFilter = document.querySelector('.sync__list');
+            showVenuesFilter.classList.toggle('show__sync')
         }
     })
 
-    $(document).on('click', function(evt) {
-        if(!$(evt.target).is('.filter__btn')) {
-            $(".filter__btn").removeClass('active__filter--option');
+    var jobFilterBtn = document.getElementById('jobFilter');
+    var jobFilterContent = document.getElementById('jobFilterContent');
+
+    var venusFilterBtn = document.getElementById('venusFilter');
+    var venusFilterContent = document.getElementById('venusFilterContent');
+
+    var syncFilterBtn = document.getElementById('syncFilter');
+    var syncFilterContent = document.getElementById('syncFilterContent');
+
+    document.addEventListener('click', function (e){
+        if (jobFilterBtn != e.target){
+            const hide = document.querySelector('.job__list');
+            hide.classList.remove('show__job')
         }
-    });
+        const so = e.target.closest('.job__list');
+        if (!so) return;
+        so.classList.add('show__job')
+    })
+
+    document.addEventListener('click', function (e){
+        if (venusFilterBtn != e.target){
+            const hide = document.querySelector('.venus__list');
+            hide.classList.remove('show__venues')
+        }
+        const so = e.target.closest('.venus__list');
+        if (!so) return;
+        so.classList.add('show__venues')
+    })
+
+    document.addEventListener('click', function (e){
+        if (syncFilterBtn != e.target){
+            const hide = document.querySelector('.sync__list');
+            hide.classList.remove('show__sync')
+        }
+        const so = e.target.closest('.sync__list');
+        if (!so) return;
+        so.classList.add('show__sync')
+    })
+
+
+
+/*    $(document).on('click', function(evt) {
+        console.log(evt.target)
+        if(!$(evt.target).is('job__list') ) {
+            console.log(evt.target)
+            // $(".job__list").removeClass('show__job');
+        }
+    });*/
 
 
     /*Action bar*/
